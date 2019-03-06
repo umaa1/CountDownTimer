53c7eac5fe10e12e67c3b91d4158eb548ee0ed4f
umaa1 <umangahasini@gmail.com>
2019-03-06 15:31:13 +0530

    test1

--- app/src/main/java/lk/ac/kln/timer/MainActivity.java
+++ app/src/main/java/lk/ac/kln/timer/MainActivity.java
@@ -1,13 +1,31 @@
 package lk.ac.kln.timer;
 
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private int counter=99;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        countDown();
     }
+    private void countDown(){
+        final TextView textView=findViewById(R.id.textView);
+        final Handler handler = new Handler();
+
+        handler.post (new Runnable() {
+            @Override
+            public void run() {
+                textView.setText(Integer.toString(counter));
+                counter--;
+                handler.postDelayed(this, 1000);
+            }
+        });
+    }
+
 }
